939b1b7d4088b77d933f516a4ea21ce91feefdc8
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-05 15:07:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,15 @@
 ##INPUTS --> speed --> number
 ##OUTPUTS --> answer the question am I unsafe
 
-# if your speed is < 40, say true
-# if your speed is > 60, say true
-
 # if your speed is > 60 OR your speed is < 40, say true
 #otherwiese, say false
 
 def unsafe?(speed)
-if spee > 60 || < 40
+if speed > 60 || speed < 40
+  return true
+ else 
+  return false 
+ end
 end
 
 
