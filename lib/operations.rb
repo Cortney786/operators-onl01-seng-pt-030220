037923eaaa1bb48932837a1d43a08cce408d4d2b
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-05 14:59:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,10 @@
 ##INPUTS --> speed --> number
 ##OUTPUTS --> answer the question am I unsafe
 
-#
+# if your speed is < 40, say true
+# if your speed is > 60, say true
+
+# if your speed is 
 
 def unsafe?(speed)
 if = > 40 || or > 60
